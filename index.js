1c97850e98d8fb6d9c8a9416dcf34fe671dba50a
Brian Kozub <ticet11@gmail.com>
2021-11-03 17:40:24 -0600

    Refactored serverto deal with more content types.

--- index.js
+++ index.js
@@ -3,39 +3,52 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	let reqUrl;
+	let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+	let extName = path.extname(filePath);
 	let contentType = "text/html";
 	let jsonData;
 
-	switch (req.url) {
-		case "/":
-			reqUrl = path.join(__dirname, "public/index.html");
+	switch (extName) {
+		case ".js":
+			contentType = "text/javascript";
 			break;
-		case "/about":
-			reqUrl = path.join(__dirname, "public/about.html");
+		case ".css":
+			contentType = "text/css";
 			break;
-		case "/api/users":
-			const users = [
-				{ name: "Brian Kozub", dob: new Date("11/25/1993") },
-				{ name: "Daisy Kozub", dob: new Date("11/25/2018") },
-			];
-			contentType = "application/json; charset=utf-8";
-			jsonData = users;
+		case ".png":
+			contentType = "image/png";
+			break;
+		case ".jpg":
+		case ".jpeg":
+			contentType = "image/jpg";
+			break;
+		case "":
+			filePath.includes("api")
+				? ((filePath += ".json"), (contentType = "application/json"))
+				: (filePath += ".html");
+			break;
 		default:
-			reqUrl = path.join(__dirname, "public/error.html");
 			break;
 	}
 
-	if (contentType === "text/html") {
-		fs.readFile(reqUrl, (err, data) => {
-			if (err) throw err;
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			if (err.code === "ENOENT") {
+				// Page not found
+				fs.readFile(path.join(__dirname, "public", "error.html"), (err, data) => {
+					res.writeHead(200, { "Content-Type": "text/html" });
+					res.end(data, "utf8");
+				});
+			} else {
+				// Server Error
+				res.writeHead(500);
+				res.end(`Server Error: ${err.message}`);
+			}
+		} else {
 			res.writeHead(200, { "Content-Type": contentType });
-			res.end(data);
-		});
-	} else {
-		res.writeHead(200, { "Content-Type": contentType });
-		res.end(JSON.stringify(jsonData));
-	}
+			res.end(data, "utf8");
+		}
+	});
 });
 
 const PORT = process.env.PORT || 5000;
